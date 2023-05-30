fb3b367a6d0acd31b8de402eece08d3b0504e3c1
TAMARA YAEL DRATLER <47807159@ort>
2023-05-30 10:40:32 -0300

    hola

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,7 +29,7 @@ public class HomeController : Controller
         if(sala == Escape.GetEstadoJuego())
         {
          if(!Escape.ResolverSala(sala, clave)) ViewBag.Error = "Resultado incorrecto";
-         if(Escape.GetEstadoJuego() >4)return View("Victoria");
+         if(Escape.GetEstadoJuego() ==4)return View("Victoria");
         }   
         return View("Habitacion" + Escape.GetEstadoJuego());
     }
